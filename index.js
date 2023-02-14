f6c5eb4fd2232bb8c00cfe54e3b82e5b64378b41
doh43 <ohdaniel43@gmail.com>
2023-02-14 17:25:57 -0500

    Create basic working bot

---
+++ index.js
@@ -0,0 +1,31 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+dotenv.config();
+
+// Create a new client instance
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+
+    ],
+});
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on("messageCreate", (message) {
+    if (message.author.bot) return;
+    return message.reply(`${message.content}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
+
+
